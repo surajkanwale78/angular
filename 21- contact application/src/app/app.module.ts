a277c28316827ff13d79d0164a7fa6e983773e43
Suraj Kanwale <59122847+surajkanwale78@users.noreply.github.com>
2022-05-05 07:57:01 +0530

    Add files via upload

---
+++ 21- contact application/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ContactAapComponent } from './component/contact-aap/contact-aap.component';
+import { ContactListComponent } from './component/contact-list/contact-list.component';
+import { ContactCardComponent } from './component/contact-card/contact-card.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactAapComponent,
+    ContactListComponent,
+    ContactCardComponent,    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
